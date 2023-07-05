7cd333b16678c1c39bbc89cf20772f1749334526
Luiz Oséas <luizoseasmm@gmail.com>
2023-07-05 01:21:27 -0300

    Modificações

--- src/main/java/br/dev/lomm/automecanicapoo/database/Cargo.java
+++ src/main/java/br/dev/lomm/automecanicapoo/database/Cargo.java
@@ -11,6 +11,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -33,7 +35,7 @@ public class Cargo implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idcargo")
     private Integer idcargo;
     @Basic(optional = false)
