f3c1ac3a2fdcc0f0571be8d679a328cfb934db50
Luiz Oséas <luizoseasmm@gmail.com>
2023-07-05 01:37:00 -0300

    Modificações

---
+++ src/main/java/Interfaces/FalhaException.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Interfaces;
+
+import Views.FormErro;
+
+/**
+ *
+ * @author oseas
+ */
+public class FalhaException extends Exception {
+
+    private static final FormErro formErro = new FormErro();
+    
+    public FalhaException(String mensagem) {
+        super(mensagem);
+        formErro.setVisible(true, mensagem);
+    }
+    
+    
+}
